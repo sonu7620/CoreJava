7635d64c8b24174a905176ec5a71afcba1915da6
sonu7620 <65360148+sonu7620@users.noreply.github.com>
2022-02-11 22:35:35 +0530

    Create Problem4.java

---
+++ Problem4.java
@@ -0,0 +1,19 @@
+//Write a Java program to find out whether the given String is Palindrome or not.
+class Problem3 {
+    public static void main(String args[]) {
+        int r, sum = 0, temp;
+        // It is the number variable to be checked for palindrome
+        int n = 454;
+        temp = n;
+        while (n > 0) {
+            // getting remainder 
+            r = n % 10;
+            sum = (sum * 10) + r;
+            n = n / 10;
+        }
+        if (temp == sum)
+            System.out.println("palindrome number ");
+        else
+            System.out.println("not palindrome");
+    }
+}
