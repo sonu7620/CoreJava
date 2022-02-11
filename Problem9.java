a87116ed6667b3c7a4623cbdf03e4b5fe159a057
sonu7620 <65360148+sonu7620@users.noreply.github.com>
2022-02-11 22:47:45 +0530

    Create Problem9.java

---
+++ Problem9.java
@@ -0,0 +1,18 @@
+//Write a Java program to remove elements from an ArrayList
+import java.util.ArrayList;
+import java.util.List;
+public class problem8 {
+    public static void main(String[] args)
+    {
+        List<Integer> al = new ArrayList<>();
+        al.add(10);
+        al.add(20);
+        al.add(30);
+        al.add(1);
+        al.add(2);
+        System.out.println(al);
+        al.remove(1);
+        al.remove(1);
+        System.out.println(al);
+    }
+}
